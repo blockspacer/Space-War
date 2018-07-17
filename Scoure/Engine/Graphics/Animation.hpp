2cb3dab423efcb9bab835a07dcc7d9c56eeedf06
HardingAdonis <hardingadonis@gmail.com>
2018-07-17 15:14:38 +0700

    Xong class Engine::Animation

--- Scoure/Engine/Graphics/Animation.hpp
+++ Scoure/Engine/Graphics/Animation.hpp
@@ -15,13 +15,46 @@ namespace Engine
     class Animation
     {
     private:
-        std::vector<sf::IntRect>       m_all_frame;
+        std::vector<sf::IntRect>       m_all_frames;
 
-        Clock                          m_clock;
+        Engine::Clock                  m_clock;
+
+        float                          m_time_next_frame;
+
+        int                            m_current_frame;
 
     public:
+        Animation() = default;
+        Animation(sf::IntRect sizeFrame, float time_next_frame, int count);
+        ~Animation();
+
+        void create(sf::IntRect sizeFrame, float time_next_frame, int count);
+
+        //   Return the current Frame
+        sf::IntRect getFrame();
 
 
+        //   Return the number of current Frame
+        int getCurrentFrame() const;
+
+
+        //   Set current frame
+        void setCurrentFrame(int countFrame);
+
+
+        //   Return the time next frame
+        float getTimeNextFrame() const;
+
+
+        //   Set time next frame
+        void setTimeNextFrame(float time_next_frame);
+
+        //   Stop Clock
+        void stop();
+
+
+        //   Continue Clock if it stoped
+        void start();
     };
 }
 
